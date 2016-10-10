0cdb83d5a383d0d64d6a974c5d36e3b5bd3b4f7c
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2016-10-10 15:34:21 +0200

    Launching modal window containing CodeMirror instance with code snippets pre-loaded from the document

--- build.sbt
+++ build.sbt
@@ -30,7 +30,7 @@ lazy val pluginSettings = Seq(
 
 lazy val micrositeSettings = Seq(
   micrositeName := "sbt-microsites",
-  micrositeDescription := "An sbt plugin to create awesome microsites for your project",
+  micrositeDescription := "A sbt plugin to create awesome microsites for your project",
   micrositeBaseUrl := "sbt-microsites",
   micrositeDocumentationUrl := "/sbt-microsites/docs/",
   micrositeGithubOwner := "47deg",
@@ -52,10 +52,22 @@ lazy val jsSettings = Seq(
     "be.doeraene" %%% "scalajs-jquery" % "0.9.0",
     "com.lihaoyi" %%% "upickle" % "0.4.1",
     "org.scala-exercises" %%% "evaluator-client" % "0.1.1-SNAPSHOT"),
+    "com.lihaoyi" %%% "scalatags"  % "0.6.0",
+    "com.github.japgolly.scalacss" %%% "core" % "0.5.0",
+    "com.github.japgolly.scalacss" %%% "ext-scalatags" % "0.5.0",
+    "org.querki" %%% "jquery-facade" % "1.0-RC6",
+    "org.denigma" %%% "codemirror-facade" % "5.11-0.7"
+  ),
+  jsDependencies ++= Seq(
+    "org.webjars" % "jquery" % "2.1.3" / "2.1.3/jquery.js",
+    ProvidedJS / "codemirror.js",
+    ProvidedJS / "javascript.js" dependsOn "codemirror.js"
+  ),
   resolvers ++= Seq(Resolver.url(
     "bintray-sbt-plugin-releases",
     url("https://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-    Resolver.sonatypeRepo("snapshots"))
+    Resolver.sonatypeRepo("snapshots"),
+    Resolver.bintrayRepo("denigma", "denigma-releases"))
 )
 
 lazy val testSettings =
--- js/src/main/scala/kazari/KazariPlugin.scala
+++ js/src/main/scala/kazari/KazariPlugin.scala
@@ -7,14 +7,22 @@ import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 import dom.ext.PimpedNodeList
 import kazari.model.EvaluatorConfig
+import org.denigma.codemirror.{CodeMirror, EditorConfiguration}
+import org.denigma.codemirror.extensions.EditorConfig
 import org.scalaexercises.evaluator.Dependency
 import org.scalaexercises.evaluator.{EvalResponse, EvaluatorClient}
 import org.scalaexercises.evaluator.EvaluatorClient._
 import org.scalaexercises.evaluator.implicits._
 import org.scalaexercises.evaluator.EvaluatorResponses.EvaluationResponse
+import org.scalajs.dom.html.Div
+import org.scalajs.dom.raw.{HTMLStyleElement, HTMLTextAreaElement}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
+import scalacss.Defaults._
+import scalacss.internal.{FontFace, NonEmptyVector}
+import scalacss.ScalatagsCss._
+import org.querki.jquery._
 
 @JSExport
 object KazariPlugin extends JSApp with DOMHelper {
@@ -22,6 +30,10 @@ object KazariPlugin extends JSApp with DOMHelper {
   lazy val codeSnippets = document.querySelectorAll(s"code.language-scala:not(.$codeExcludeClass)")
   val dependenciesMetaName = "evaluator-dependencies"
   val resolversMetaName = "evaluator-resolvers"
+  val codeModalClass = "modalDialog"
+  val codeModalCloseButtonClass = "closeButton"
+  val codeModalInternalTextArea = "modalInternalTextArea"
+  val codeModalButtonContainer = "modalButton"
 
   @JSExport
   def main(): Unit = { }
@@ -29,22 +41,46 @@ object KazariPlugin extends JSApp with DOMHelper {
   @JSExport
   def decorateCode(config: EvaluatorConfig): Unit = {
     val textSnippets = generateCodeTextSnippets()
+    lazy val evalClient = new EvaluatorClient(
+      config.url,
+      config.authToken)
+
+    val modalDiv = createModalDiv(codeModalClass)
+    document.body.appendChild(modalDiv)
+    document
+        .querySelector("." + codeModalCloseButtonClass)
+        .addEventListener("click", { (e: dom.MouseEvent) =>
+          $("#" + codeModalClass).toggleClass("ModalStyles-default").toggleClass("ModalStyles-active")
+    })
+    insertCSSStyleSheet("codemirror.css")
+    insertCSSStyleSheet("monokai.css")
+    val cmParams: EditorConfiguration = EditorConfig
+        .mode("javascript")
+        .lineNumbers(true)
+        .theme("monokai")
+    document.querySelector("#" + codeModalInternalTextArea) match {
+      case el: HTMLTextAreaElement =>
+        val m = CodeMirror.fromTextArea(el, cmParams)
+        m.getDoc().setValue(textSnippets.last)
+        val containerDiv = document.querySelector("#" + codeModalButtonContainer)
+        appendButton(containerDiv, "Evaluate", onClickFunction = (e: dom.MouseEvent) => {
+          val snippet = m.getDoc().getValue()
+          val evalResponse = sendEvaluatorRequest(evalClient, snippet)
+          evalResponse onComplete  {
+            case Success(r) ⇒
+              println(s"Connection to evaluator established: $r")
+            case Failure(f) ⇒
+              println(s"Error while connecting with remote evaluator: $f")
+          }
+        })
+      case _ => console.error("Couldn't find text area to embed CodeMirror instance!")
+    }
 
     codeSnippets.zipWithIndex foreach { case (node, i) =>
       appendButton(node, "Evaluate", onClickFunction = (e: dom.MouseEvent) => {
         val snippet = textSnippets.lift(i + 1)
-
-        val client = new EvaluatorClient(
-          config.url,
-          config.authToken)
-
-        snippet.foreach((s: String) => {
-          val evalResponse: Future[EvaluationResponse[EvalResponse]] =
-            client.api.evaluates(
-              dependencies = getDependenciesList(),
-              resolvers = getResolversList(),
-              code = snippet.getOrElse("")).exec
-
+        snippet foreach( (s: String) => {
+          val evalResponse = sendEvaluatorRequest(evalClient, s)
           evalResponse onComplete  {
             case Success(r) ⇒
               println(s"Connection to evaluator established: $r")
@@ -53,12 +89,15 @@ object KazariPlugin extends JSApp with DOMHelper {
           }
         })
       })
+      appendButton(node, "Edit", onClickFunction = (e: dom.MouseEvent) => {
+        $("#" + codeModalClass).toggleClass("ModalStyles-default").toggleClass("ModalStyles-active")
+      })
     }
   }
 
   def generateCodeTextSnippets() = {
     codeSnippets.map(_.textContent)
-        .scanLeft("")((currentItem, result) => currentItem + result)
+        .scanLeft("")((currentItem, result) => currentItem + "\n" + result)
   }
 
   def getMetaContent(metaTagName: String): String = {
@@ -88,6 +127,12 @@ object KazariPlugin extends JSApp with DOMHelper {
     val content = getMetaContent(resolversMetaName)
     content.split(",").toList
   }
+
+  def sendEvaluatorRequest(evaluator: EvaluatorClient, codeSnippet: String): Future[EvaluationResponse[EvalResponse]] =
+    evaluator.api.evaluates(
+        dependencies = getDependenciesList(),
+        resolvers = getResolversList(),
+        code = codeSnippet).exec
 }
 
 trait DOMHelper {
@@ -99,4 +144,92 @@ trait DOMHelper {
     btnNode.addEventListener("click", onClickFunction)
     targetNode.appendChild(btnNode)
   }
+
+  def createModalDiv(cssClass: String): Div = {
+    import scalatags.JsDom.all._
+    div(
+      `id` := cssClass,
+      `class` := cssClass,
+      ModalStyles.render[scalatags.JsDom.TypedTag[HTMLStyleElement]],
+      ModalStyles.default,
+      div(
+        ModalStyles.internalDiv,
+        a(
+          title := "Close",
+          `class` := KazariPlugin.codeModalCloseButtonClass,
+          ModalStyles.closeButton,
+          "X"
+        ),
+        textarea(
+          `id` := KazariPlugin.codeModalInternalTextArea
+        ),
+        div(
+          `id` := KazariPlugin.codeModalButtonContainer
+        )
+      )
+    ).render
+  }
+
+  def insertCSSStyleSheet(name: String) = {
+    val link = document.createElement("link")
+    link.setAttribute("rel", "stylesheet")
+    link.setAttribute("type", "text/css")
+    link.setAttribute("href", name)
+    document.head.appendChild(link)
+  }
+}
+
+object ModalStyles extends StyleSheet.Inline {
+  import dsl._
+
+  val common = style(
+    position fixed,
+    fontFamily(FontFace("Arial, Helvetica, sans-serif", NonEmptyVector(""))),
+    top(0 px),
+    bottom(0 px),
+    right(0 px),
+    left(0 px),
+    backgroundColor(rgba(0, 0, 0, 0.8)),
+    transition := "opacity 400ms ease-in"
+  )
+
+  val default = style(
+    common,
+    pointerEvents := none,
+    zIndex(99999),
+    opacity(0)
+  )
+
+  val active = style(
+    common,
+    opacity(1),
+    pointerEvents := auto
+  )
+
+  val internalDiv = style(
+    width(600 px),
+    position relative,
+    margin(10 %%, auto),
+    padding(5 px, 20 px, 13 px, 20 px),
+    borderRadius(10 px),
+    backgroundColor(c"#fff")
+  )
+
+  val closeButton = style(
+    backgroundColor(c"#606061"),
+    color(c"#FFFFFF"),
+    lineHeight(25 px),
+    position absolute,
+    right(-12 px),
+    textAlign center,
+    top(-10 px),
+    width(24 px),
+    textDecoration := ^.none,
+    fontWeight bold,
+    borderRadius(12 px),
+    boxShadow := "1px 1px 3px #000",
+    &.hover(
+       backgroundColor(c"#00d9ff")
+    )
+  )
 }
