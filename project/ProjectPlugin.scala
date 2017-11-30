c40912eb8e46094e84a5d5a812013e508ed4f5f6
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2017-11-30 18:07:54 +0100

    Upgrade sbt-org-policies version dependencies to 0.8.17

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -27,7 +27,7 @@ object ProjectPlugin extends AutoPlugin {
       addSbtPlugin(%("sbt-ghpages", true)),
       addSbtPlugin(%("sbt-site", true)),
       libraryDependencies ++= Seq(
-        %%("org-policies-core", "0.8.14"),
+        %%("org-policies-core", "0.8.17"),
         %%("moultingyaml"),
         %%("scalatags"),
         %%("scalactic"),
