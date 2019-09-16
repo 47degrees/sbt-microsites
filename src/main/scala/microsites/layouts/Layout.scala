38210765bfb02d71d03756cca724584bef650da3
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-09-16 15:44:50 +0200

    Fix library name

--- src/main/scala/microsites/layouts/Layout.scala
+++ src/main/scala/microsites/layouts/Layout.scala
@@ -33,8 +33,8 @@ object Layout {
       a(
         href := s"https://47deg.github.io/sbt-microsites/",
         target := "_blank",
-        s"Sbt-microsites"
         rel := "noopener noreferrer",
+        s"sbt-microsites"
       ),
       s" - © 2016 ",
       a(
