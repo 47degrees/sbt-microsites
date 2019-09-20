ad1e88672f30584d5abfcf759b3d83cde4d9233c
Scala Steward <me@scala-steward.org>
2019-09-20 19:02:39 +0200

    Update moultingyaml to 0.4.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val V = new {
       val mdoc: String         = "1.3.2"
-      val moultingyaml: String = "0.4.0"
+      val moultingyaml: String = "0.4.1"
       val orgPolicies: String  = "0.12.0-M2"
       val scala: String        = "2.12.9"
       val scalactic: String    = "3.0.8"
