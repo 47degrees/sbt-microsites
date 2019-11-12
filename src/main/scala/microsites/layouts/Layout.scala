f8a2b253efa9e347ebed3e50a1ed6543258160b1
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-11-12 17:44:00 +0100

    Replace Bootstrap Font Awesome CDN URL to use Cloudflare standard one

--- src/main/scala/microsites/layouts/Layout.scala
+++ src/main/scala/microsites/layouts/Layout.scala
@@ -168,7 +168,7 @@ abstract class Layout(config: MicrositeSettings) {
         List(
           link(
             rel := "stylesheet",
-            href := "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"),
+            href := "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
           link(
             rel := "stylesheet",
             href := s"{{site.url}}{{site.baseurl}}/highlight/styles/${config.visualSettings.highlightTheme}.css"),
